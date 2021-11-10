df95207d93f90e648325c109d66e03b7e4767219
Mariana Lozynska <marianalozynska@YPs-iMac.local>
2021-11-10 21:20:58 +0000

    hydepark

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './components/Home.js'
+// import NavBarHome from './components/NavBarHome'
+import NavBar from './components/NavBar.js'
+import ParkIndex from './components/ParkIndex.js'
 
 const App = () => {
 
   return (
-    <h1> Hello World</h1>
+    <BrowserRouter >
+    <NavBar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/parks' component={ParkIndex} />
+      </Switch>
+    </BrowserRouter>
   )
 }
 
