22f362c9cac363c9c1fb58dd8b207d87b4613db8
piotr <piotr.jankowski2204@gmail.com>
2021-11-10 21:11:23 +0000

    login,register,user profile added to front end

--- back-end/config/router.js
+++ back-end/config/router.js
@@ -3,6 +3,8 @@ import { createPark, deletePark, editingPark, getAllParks, getParkByID } from '.
 import { addComment, deleteComment, showComments } from '../controlers/comments.js'
 import { loginUser, registerUser } from '../controlers/auth.js'
 import { secureRoute } from '../config/secureRoute.js'
+import { userProfile } from '../controlers/userProfile.js'
+import { getAllUsers } from '../controlers/users.js'
 
 const router = express.Router()
 
@@ -20,6 +22,12 @@ router.route('/login')
 
 router.route('/register')
   .post(registerUser)
+
+router.route('/allusers')
+  .get(getAllUsers)
+
+router.route('/profile')
+  .get(secureRoute,userProfile)
 
 router.route('/london-parks-api/:id/comments')
   .post(secureRoute,addComment)
--- back-end/models/parks_model.js
+++ back-end/models/parks_model.js
@@ -24,6 +24,23 @@ const parkSchema = new mongoose.Schema({
   comments: [commentSchema]
 })
 
+// * RATINGS
+// * virtual getter -> virtual field, added to the object before its returned as JSON
+parkSchema.virtual('averageRating') // * define name of new key for the virtual field
+  .get(function() {
+    // * if there are no comments, return a string
+    if (!this.comments.length) return 'Not rated yet'
+    // * iterate through the comments array, add up all of the ratings
+    const sumOfRatings = this.comments.reduce((acc, comment) => {
+      if (!comment.rating) return acc
+      return acc + comment.rating
+    }, 0)
+    // * return the average of the ratings, fixed to 2 decimal places
+    return (sumOfRatings / this.comments.length).toFixed(2)
+  })
+
+// * make sure that when show data is returned as JSON, virtual fields are included
+parkSchema.set('toJSON', { virtuals: true }) 
 parkSchema.plugin(uniqueValidator)
 
 export default  mongoose.model('Park', parkSchema) 
